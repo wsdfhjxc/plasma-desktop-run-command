6545b1c3517f3a2f151ca95cfcfcd9a6f7e084c1
wsdfhjxc <wsdfhjxc@op.pl>
2020-12-19 12:31:33 +0100

    Update helper script

--- helper.sh
+++ helper.sh
@@ -2,7 +2,7 @@
 
 printUsage() {
     echo "Usage: helper.sh build|install|uninstall"
-    echo "   or: helper.sh installBuildDependencies ubuntu"
+    echo "   or: helper.sh installBuildDependencies ubuntu|fedora|opensuse|arch"
 }
 
 build() {
@@ -41,7 +41,19 @@ uninstall() {
 installBuildDependencies() {
     case $1 in
         ubuntu)
-            sudo apt install cmake extra-cmake-modules qtbase5-dev libkf5plasma-dev
+            sudo apt install g++ cmake extra-cmake-modules qtbase5-dev libkf5plasma-dev
+            ;;
+
+        fedora)
+            sudo dnf install gcc-c++ cmake extra-cmake-modules qt5-qtbase-devel kf5-plasma-devel
+            ;;
+
+        opensuse)
+            sudo zypper in gcc-c++ cmake extra-cmake-modules libqt5-qtbase-devel plasma-framework-devel
+            ;;
+
+        arch)
+            sudo pacman -S gcc cmake extra-cmake-modules
             ;;
 
         *)
